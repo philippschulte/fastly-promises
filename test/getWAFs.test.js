71153cb01a6b51e759797883aef2e7e0db5548df
Eric.Anand <eric.anand@ft.com>
2018-02-27 13:24:21 +0000

    Addition of tests for getWAFRules

--- test/getWAFs.test.js
+++ test/getWAFs.test.js
@@ -1,16 +1,13 @@
 'use strict';
 
-require('dotenv').config({path: require('path').join(__dirname, '../src/.env')});
 const nock = require('nock');
 const expect = require('expect');
 const config = require('../src/config');
 const fastlyPromises = require('../src/index');
 const response = require('./response/getWAFs.response');
 
-const FASTLY_API_TOKEN = process.env.FASTLY_API_TOKEN;
-
 describe('#getWAFs', () => {
-  const fastly = fastlyPromises(FASTLY_API_TOKEN, '79CEhEeP8DKPQQGTXokV5M');
+  const fastly = fastlyPromises('923b6bd5266a7f932e41962755bd4254', '79CEhEeP8DKPQQGTXokV5M');
   let res;
 
   nock(config.mainEntryPoint)
@@ -19,12 +16,10 @@ describe('#getWAFs', () => {
 
   before(async () => {
     res = await fastly.getWAFs('9');
-    console.log(res.data)
   });
 
   it('response should be a status 200', () => {
     expect(res.status).toBe(200);
-    //console.log(res.data.data)
   });
 
   it('response body should exist', () => {
