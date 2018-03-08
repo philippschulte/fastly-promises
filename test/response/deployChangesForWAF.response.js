cc20b9e2e009e67aab2853fe579e0728489a039c
Eric.Anand <eric.anand@ft.com>
2018-03-08 10:16:25 +0000

    Activate method with tests.

---
+++ test/response/deployChangesForWAF.response.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports.deployChangesForWAF =
+{
+   "data":{
+      "id":"rfjn9II8V21LSeEgyMT9x",
+      "type":"ruleset"
+   },
+   "links":{
+      "related":{
+         "href":"https://api.fastly.com/service/79CEhEeP8DKPQQGiXokV5M/wafs/rfjm9II8V21LSeEgyMi9x/update_statuses/4N0FXLCdRDk8KkyAZ6Bd8N"
+      }
+   }
+};
