9ca4a2c9c97c752fbbf0326d6dd592e6b6958b4d
Lars Trieloff <trieloff@adobe.com>
2019-01-09 11:20:42 +0000

    fix(errors): Fix 404 Error Handling
    Error 404 coming from the Fastly API were not handled correctly
    
    #14

--- src/httpclient.js
+++ src/httpclient.js
@@ -1,5 +1,15 @@
 const request = require('request-promise-native');
 
+class FastlyError extends Error {
+  constructor(response) {
+    super(response.body.detail);
+
+    this.data = response.body;
+    this.status = response.statusCode;
+    this.code = response.body.msg;
+  }
+}
+
 function create({ baseURL, timeout, headers }) {
   function makereq(method) {
     return function req(path, body, config) {
@@ -17,14 +27,19 @@ function create({ baseURL, timeout, headers }) {
         simple: false,
       };
 
-      return request(options).then(response => ({
-        status: response.statusCode,
-        statusText: response.stausMessage,
-        headers: response.headers,
-        config: options,
-        request: response.request,
-        data: response.body,
-      }));
+      return request(options).then((response) => {
+        if (response.statusCode >= 400) {
+          throw new FastlyError(response);
+        }
+        return {
+          status: response.statusCode,
+          statusText: response.stausMessage,
+          headers: response.headers,
+          config: options,
+          request: response.request,
+          data: response.body,
+        };
+      });
     };
   }
 
--- test/readS3.404.test.js
+++ test/readS3.404.test.js
@@ -11,33 +11,50 @@ const response = require('./response/readS3.response');
 describe('#readS3', () => {
   const fastly = fastlyPromises('923b6bd5266a7f932e41962755bd4254', 'SU1Z0isxPaozGVKXdv0eY');
   let res;
+  let err;
 
   nock(config.mainEntryPoint)
     .get('/service/SU1Z0isxPaozGVKXdv0eY/version/1/logging/s3/test-s3-does-not-exist')
     .reply(404, response.readS3404);
 
   before(async () => {
-    res = await fastly.readS3('1', 'test-s3-does-not-exist');
+    err = undefined;
+    try {
+      res = await fastly.readS3('1', 'test-s3-does-not-exist');
+    } catch (e) {
+      err = e;
+    }
   });
 
   it('response should be a status 404', () => {
-    expect(res.status).toBe(404);
+    expect(res).not.toBeDefined();
+    expect(err instanceof Error).toBeTruthy();
+    expect(err.status).toBe(404);
   });
 
-  it('response body should exist', () => {
-    expect(res.data).toBeTruthy();
+  it('error body should exist', () => {
+    expect(err.data).toBeTruthy();
   });
 
-  it('response body should be an object', () => {
-    expect(typeof res.data).toBe('object');
+  it('error code should exist', () => {
+    expect(err.code).toBeTruthy();
+    expect(err.code).toBe('Record not found');
   });
 
-  it('response body should contain all properties', () => {
+  it('error message should exist', () => {
+    expect(err.message).toBeTruthy();
+  });
+
+  it('error body should be an object', () => {
+    expect(typeof err.data).toBe('object');
+  });
+
+  it('response err should contain all properties', () => {
     [
       'msg',
       'detail',
     ].forEach((e) => {
-      expect(Object.keys(res.data)).toContain(e);
+      expect(Object.keys(err.data)).toContain(e);
     });
   });
 });
