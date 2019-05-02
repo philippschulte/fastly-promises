6e3726c6399541e22dc473e8df785752e0a3a5a5
Lars Trieloff <trieloff@adobe.com>
2019-05-02 09:04:06 +0000

    fix(httpclient): do not memoize failing requests
    Replaces the lodash.memoize function with one that makes sure not to memoize failing requests (e.g.
    404 responses)
    
    fixes #61

--- test/httpclient.test.js
+++ test/httpclient.test.js
@@ -6,6 +6,53 @@ const expect = require('expect');
 const assert = require('assert');
 const httpclient = require('../src/httpclient');
 
+describe('#httpclient.caches', () => {
+  it('Status 200 can get cached', async () => {
+    const scope = nock('http://www.example.com/')
+      .get('/foo')
+      .reply(200, 'first')
+      .get('/foo')
+      .reply(200, 'second');
+
+    const client = httpclient.create({
+      baseURL: 'http://www.example.com',
+      headers: {},
+      timeout: 1000,
+    });
+
+    const first = await client.get('/foo');
+    const second = await client.get('/foo');
+
+    expect(first.data).toEqual(second.data);
+    expect(scope.isDone()).toBeFalsy();
+  });
+
+  it('Status 404 will not get cached', async () => {
+    const scope = nock('http://www.example.com/')
+      .get('/bar')
+      .reply(404, 'foo')
+      .get('/bar')
+      .reply(200, 'second');
+
+    const client = httpclient.create({
+      baseURL: 'http://www.example.com',
+      headers: {},
+      timeout: 1000,
+    });
+
+    try {
+      const first = await client.get('/bar');
+      expect(first).not.toBeDefined();
+    } catch (e) {
+      expect(e.name).toEqual('FastlyError');
+    }
+    const second = await client.get('/bar');
+    expect(second.data).toEqual('second');
+
+    expect(scope.isDone()).toBeTruthy();
+  });
+});
+
 describe('#httpclient.retries', () => {
   it('Error 429 triggers a retry', async () => {
     const scope = nock('http://www.example.com/')
