fbd0c6096c87e99f6862b4a0d1b9af499651509a
Lars Trieloff <trieloff@adobe.com>
2019-08-20 12:58:17 +0000

    fix(clone): if no active version can be found during clone fall back to current, latest, or initial
    fixes #106

--- src/index.js
+++ src/index.js
@@ -516,7 +516,7 @@ class Fastly {
    * @returns {Promise} The response object representing the completion or failure.
    */
   async cloneVersion(version) {
-    const versions = await this.request.put(`/service/${this.service_id}/version/${await this.getVersion(version, 'active')}/clone`);
+    const versions = await this.request.put(`/service/${this.service_id}/version/${await this.getVersion(version, 'active', 'current', 'latest', 'initial')}/clone`);
     this.versions.current = versions.data.number;
     this.versions.latest = versions.data.number;
     return versions;
