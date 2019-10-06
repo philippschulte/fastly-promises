588b10a7db83329686bf23f6e0089a622afd68b5
Prashanth <hi@prash.xyz>
2019-10-06 19:07:32 +0800

    style: fix healthcheck capitalization

--- src/index.js
+++ src/index.js
@@ -1239,52 +1239,52 @@ class Fastly {
 
   /**
    * List all healthchecks for a particular service and version.
-   * 
+   *
    * @param {string} version - The current version of a service.
    * @returns {Promise} The response object representing the completion or failure.
    */
-  async readHealthChecks(version) {
+  async readHealthchecks(version) {
     return this.request.get(`/service/${this.service_id}/version/${await this.getVersion(version, 'current')}/healthcheck`);
   }
 
   /**
    * Get details of a single named healthcheck.
-   * 
+   *
    * @param {string} version - The current version of a service.
    * @param {string} name - The name of the healthcheck.
    * @returns {Promise} The response object representing the completion or failure.
    */
-  async readHealthCheck(version, name) {
+  async readHealthcheck(version, name) {
     return this.request.get(`/service/${this.service_id}/version/${await this.getVersion(version, 'current')}/healthcheck/${name}`);
   }
 
   /**
    * Create a healthcheck for a particular service and version.
-   * 
+   *
    * @param {string} version - The current version of a service.
    * @param {object} data - The healthcheck definition.
    * @returns {Promise} The response object representing the completion or failure.
    */
-  async createHealthCheck(version, data) {
-    return this.request.post(`/service/${this.service_id}/version/${await this.getVersion(version, 'current')}/healthcheck/`, data);
+  async createHealthcheck(version, data) {
+    return this.request.post(`/service/${this.service_id}/version/${await this.getVersion(version, 'current')}/healthcheck`, data);
   }
 
   /**
    * Update the healthcheck for a particular service and version.
-   * 
+   *
    * @param {string} version - The current version of a service.
    * @param {string} name - The name of the healthcheck to update.
    * @param {object} data - The healthcheck definition.
    * @returns {Promise} The response object representing the completion or failure.
    */
-  async updateHealthCheck(version, name, data) {
+  async updateHealthcheck(version, name, data) {
     return this.request.put(`/service/${this.service_id}/version/${await this.getVersion(version, 'current')}/healthcheck/${name}`, data);
   }
 
   /**
    * Delete the healthcheck for a particular service and version.
-   * 
-   * @param {string} version The current version of a service.
+   *
+   * @param {string} version - The current version of a service.
    * @param {string} name - The name of the healthcheck to delete.
    * @returns {Promise} The response object representing the completion or failure.
    */
