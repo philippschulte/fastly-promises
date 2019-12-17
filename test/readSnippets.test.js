eef35307b314483975eb682394cb6aed7ac54dc8
Lars Trieloff <trieloff@adobe.com>
2019-12-17 12:04:00 +0000

    test(readsnippets): add tests for readSnippets method
    adding tests for @nullvariable, as discussed here: https://github.com/adobe/fastly-native-promises/pull/173#issuecomment-565747226

---
+++ test/readSnippets.test.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+'use strict';
+
+/* eslint-env mocha */
+
+const nock = require('nock');
+const expect = require('expect');
+const config = require('../src/config');
+const fastlyPromises = require('../src/index');
+const response = require('./response/readSnippets.response');
+
+describe('#readSnippets', () => {
+  const fastly = fastlyPromises('923b6bd5266a7f932e41962755bd4254', 'SU1Z0isxPaozGVKXdv0eY');
+  let res;
+
+  nock(config.mainEntryPoint)
+    .get('/service/SU1Z0isxPaozGVKXdv0eY/version/182/snippet')
+    .reply(200, response.readSnippets);
+
+  before(async () => {
+    res = await fastly.readSnippets('182');
+  });
+
+  it('response should be a status 200', () => {
+    expect(res.status).toBe(200);
+  });
+
+  it('response body should exist', () => {
+    expect(res.data).toBeTruthy();
+  });
+
+  it('response body should be an array', () => {
+    expect(Array.isArray(res.data)).toBe(true);
+  });
+
+  it('response body should be an array of objects', () => {
+    res.data.forEach((item) => {
+      expect(typeof item).toBe('object');
+    });
+  });
+
+  it('response body should contain all properties', () => {
+    res.data.forEach((item) => {
+      ['id', 'service_id', 'version', 'name', 'priority', 'dynamic', 'type', 'content', 'created_at', 'updated_at', 'deleted_at'].forEach((e) => {
+        expect(Object.keys(item)).toContain(e);
+      });
+    });
+  });
+});
---
+++ test/response/readSnippets.response.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.readSnippets = [
+  {
+    id: '62Yd1WfiCBPENLloXfXmlO',
+    service_id: 'SU1Z0isxPaozGVKXdv0eY',
+    version: '182',
+    name: 'my_snippet',
+    priority: '100',
+    dynamic: '1',
+    type: 'hit',
+    content: null,
+    created_at: '2016-08-15T09:37:10+00:00',
+    updated_at: '2016-08-15T09:37:10+00:00',
+    deleted_at: null,
+  },
+];
