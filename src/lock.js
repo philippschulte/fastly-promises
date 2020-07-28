ad3c24c669fae825a5870b469761d90bc7cf8444
Lars Trieloff <trieloff@adobe.com>
2020-07-28 08:44:12 +0000

    feat(lock): add a lock helper to control concurrency

---
+++ src/lock.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* eslint-env mocha */
+
+class Lock {
+  constructor() {
+    this._acquired = false;
+    this._waiting = [];
+  }
+
+  async acquire() {
+    if (this._acquired) {
+      return new Promise((resolve) => this._waiting.push(resolve));
+    }
+
+    this._acquired = true;
+    return Promise.resolve();
+  }
+
+  release() {
+    if (this._waiting.length > 0) {
+      const resolve = this._waiting.shift();
+      resolve();
+    } else {
+      this._acquired = false;
+    }
+  }
+}
+
+module.exports = { Lock };
